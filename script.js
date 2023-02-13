1b0ac1d58981eef10e5d3f28fd6f129b7c22ebd0
E Susaki <103632982+susaki2002@users.noreply.github.com>
2023-02-13 12:41:58 +0900

    サンプルのhtml, JSファイルを追加（内容は消したりしても全然大丈夫です）

---
+++ script.js
@@ -0,0 +1,11 @@
+minInput=document.getElementById("minInput");
+secInput=document.getElementById("secInput");
+inputArea=document.getElementById("inputArea");
+outputArea=document.getElementById("outputArea");
+
+function timeButton(){
+    if (minInput.value && secInput.value){
+        window.alert(minInput.value+"分"+secInput.value+"秒");
+        outputArea.innerHTML='<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/632LyhbfQA5t7p7SHAjeJT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
+    }
+}
