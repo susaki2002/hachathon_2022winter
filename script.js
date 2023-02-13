d0c7d75633ac97d8172a01e1991e1f52b18e0c7c
E Susaki <103632982+susaki2002@users.noreply.github.com>
2023-02-13 12:50:03 +0900

    ファイル書き換えのテスト
    「です」を追加

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ outputArea=document.getElementById("outputArea");
 
 function timeButton(){
     if (minInput.value && secInput.value){
-        window.alert(minInput.value+"分"+secInput.value+"秒");
+        window.alert(minInput.value+"分"+secInput.value+"秒です");
         outputArea.innerHTML='<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/632LyhbfQA5t7p7SHAjeJT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
     }
 }
